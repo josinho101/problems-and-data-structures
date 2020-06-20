ecb345bf740e68684d76e0ab49fc07bf3f477486
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-06-20 06:28:23 +0530

    Added fib and spiral matrix problems

---
+++ fib/index.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+module.exports = fib;
+
+/*
+function fib(n) {
+    let results = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        let a = results[i - 1];
+        let b = results[i - 2];
+
+        results.push(a + b);
+    }
+
+    return results[n];
+}*/

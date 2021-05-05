0e397635ba1a2bae83053c17e859a538b8cfdd9a
Joseph Johny Emmatty <josinho.seven@gmail.com>
2021-05-05 06:16:17 +0530

    function to generate tree from inorder & preorder

--- bt-construction/index.js
+++ bt-construction/index.js
@@ -13,7 +13,37 @@ class Node {
 }
 
 class TreeGenerator {
-  fromPreorder(inorder, preorder) {}
+  fromPreorder(inorder, preorder, tree = null) {
+    if (preorder.length === 0 || inorder.length === 0) {
+      return null;
+    }
+
+    let root = null;
+
+    // tree will be null only when we start iteration
+    if (tree === null) {
+      root = preorder[0];
+    } else {
+      // find which element from inorder will be first in preorder
+      for (let i = 0; i <= preorder.length; i++) {
+        if (inorder.includes(preorder[i])) {
+          root = preorder[i];
+          break;
+        }
+      }
+    }
+
+    const node = new Node(root);
+    const index = inorder.indexOf(root);
+
+    const leftItems = inorder.slice(0, index);
+    node.left = this.fromPreorder(leftItems, preorder, node);
+
+    const rightItems = inorder.slice(index + 1, inorder.length);
+    node.right = this.fromPreorder(rightItems, preorder, node);
+
+    return node;
+  }
 
   fromPostorder(inorder, postorder) {}
 }
--- bt-construction/test.js
+++ bt-construction/test.js
@@ -3,4 +3,18 @@ const TreeGenerator = require("./index").TreeGenerator;
 
 test("Binary tree form Preorder & Inorder", () => {
   const generator = new TreeGenerator();
+
+  const inorder = [3, 7, 8, 10, 11, 15, 16];
+  const preOrder = [10, 7, 3, 8, 15, 11, 16];
+  const node = generator.fromPreorder(inorder, preOrder);
+
+  expect(node.data).toEqual(10);
+  expect(node.left.data).toEqual(7);
+  expect(node.right.data).toEqual(15);
+  expect(node.left.left.data).toEqual(3);
+  expect(node.left.right.data).toEqual(8);
+  expect(node.right.left.data).toEqual(11);
+  expect(node.right.right.data).toEqual(16);
+  expect(node.right.left.left).toEqual(null);
+  expect(node.right.left.right).toEqual(null);
 });

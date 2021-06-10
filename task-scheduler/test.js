130a00cb7fa279e84e47c7a888a49c05f4fcb01b
Joseph Johny Emmatty <josinho.seven@gmail.com>
2021-06-10 06:19:21 +0530

    Task scheduler

--- task-scheduler/test.js
+++ task-scheduler/test.js
@@ -8,7 +8,7 @@ test("should get 8 as interval", () => {
   expect(result).toEqual(8);
 });
 
-test("should get 8 as interval", () => {
+test("should get 16 as interval", () => {
   const tasks = ["A", "A", "A", "A", "A", "A", "B", "C", "D", "E", "F", "G"];
   const n = 2;
 
